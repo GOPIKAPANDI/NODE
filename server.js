c19a3329a916d229539a4435650a9fd7a3a33588
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2023-04-05 12:04:56 +1030

    node_basics_completed

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 const http = require("http");
 const port= 5500;
+
+//HTTP METHODS
+/* 
+>> GET : Inorder to get the data from server
+>> POST : Sending or transfering data to the server
+>> DELETE : To delete data from database
+>> PATCH : Updating certain (OR) minimum fields(To update small things)
+>> PUT : Full update(To update large things)
+*/
+
+const toDoList =["learn","apply things","succeed"];
+
 http
 .createServer((req,res)=>{
-      res.writeHead(200, {"Content-Type":"text/html"});
-      res.write("<h2>Hey Server started :-) </h2>");
-      res.end();
+    const {method, url}= req;
+    //console.log(method, url);
+    if(url==="/todos"){
+      if(method==="GET"){
+         res.writeHead(200, {"Content-Type":"text/html"}); 
+         res.write(toDoList.toString());
+        }
+        else if(method=== "POST"){
+         let body="";
+         req
+        .on('error',(err)=>{
+         console.log(err);        
+        })
+        .on('data',(chunk)=>{
+          body+=chunk;      // added to body in string format
+          console.log(chunk);
+        })
+        .on('end',()=>{
+          body=JSON.parse(body);
+          let newToDo= toDoList;
+          newToDo.push(body.item);
+          //console.log("data :",body);
+        })
+      }
 
+        else{
+          res.writeHead(501);
+        }
+    }
+    else{
+      res.writeHead(404);
+    }
+    res.end();
+    
 })
 .listen(port,()=>{   
     
